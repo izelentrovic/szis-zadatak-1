afa8a86968a854cf14a56532c73ea5e520a73667
izelentrovic <bluez.du@gmail.com>
2021-11-02 17:11:33 +0100

    dodana tekstualna logika za navigaciju

--- index.py
+++ index.py
@@ -1,1 +1,15 @@
-print("hello world")
+def main():
+    print("Dobrodošli u Unidu sustav!")
+    print("Za prijavu upišite broj 1, za registraciju broj 2:")
+
+    unos = 0
+
+    while(unos!=1 and unos!=2):
+        unos = int(input("Unesite broj: "))
+
+    email = input("Unesi email: ")
+    lozinka = input("Unesi lozinku: ")
+
+
+if __name__=="__main__":
+    main()
