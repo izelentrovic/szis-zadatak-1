5a4abe1a96e55e87d42760d1d5d7bb9636a339d8
izelentrovic <bluez.du@gmail.com>
2021-11-09 17:26:35 +0100

    implemented user registration

--- index.py
+++ index.py
@@ -1,3 +1,6 @@
+import sqlite3
+import time
+
 def main():
     print("Dobrodošli u Unidu sustav!")
     print("Za prijavu upišite broj 1, za registraciju broj 2:")
@@ -8,8 +11,24 @@ def main():
         unos = int(input("Unesite broj: "))
 
     email = input("Unesi email: ")
-    lozinka = input("Unesi lozinku: ")
+    password = hash_pass(input("Unesi lozinku: "))
+    
+    if unos == 2:
+        name = input("Unesi ime: ")
+        contact = input("Unesi kontakt broj: ")
+    
+        register_user(name, email, password, contact)
 
+def register_user(name, email, password, contact):
+    created_at = int(time.time())
+    cur.execute("""INSERT INTO user (name, email, password, contact, created_at)
+                    VALUES (?, ?, ?, ?, ?)""", (name, email, password, contact, created_at))
+    con.commit()
+
+def hash_pass(password):    # implement this
+    return password
 
 if __name__=="__main__":
+    con = sqlite3.connect('baza.db')
+    cur = con.cursor()
     main()
