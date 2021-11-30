6816bd42646f08341886b48eb64896e68a858c03
izelentrovic <bluez.du@gmail.com>
2021-11-30 17:31:09 +0100

    WIP commit

--- index.py
+++ index.py
@@ -3,120 +3,143 @@ import sqlite3
 from datetime import date, datetime, timedelta
 import time
 
-con = sqlite3.connect('baza.db')
+class Account():
+    def __init__(self):
+        self.con = sqlite3.connect('baza.db')
+        self.cur = self.con.cursor()
 
+    def __del__(self):
+        self.con.close()
 
-def login():
-        ime = input("Unesi ime: ")
-        lozinka = input("Unesi lozinku: ")
+    def login(self):
+            ime = input("Unesi ime: ")
+            lozinka = input("Unesi lozinku: ")
+
+            row=self.cur.execute("SELECT id, password FROM users WHERE name = ?", (ime,))
+            row=self.cur.fetchone()
+            uid = row[0]
+            password = row[1]
+
+            if(self.cur.rowcount<=0):
+                print(uid, password)
+                return
+            
+            print("Korisnik postoji i prijavljen \n")
+            now = date.today()
+
+            logins = 0
+
+            self.cur.execute('''UPDATE logins 
+                        SET logins = ?,
+                        last_login = ?
+                        WHERE uid = ?
+                        ''', (logins+1, now, uid))
+
+            self.cur.execute('''INSERT INTO logins
+                        (logins, last_login, uid)
+                        VALUES (?, ?, ?)
+                        ''', (logins+1, now, uid))
+
+            self.cur.execute(f'''INSERT OR REPLACE INTO logins 
+                        (logins, last_login, uid) 
+                        VALUES (
+                            (SELECT logins FROM logins WHERE Name = "SearchName"),
+                            {now},
+                            IFNULL(
+                                (SELECT COUNT FROM logins WHERE Name = "SearchName")
+                                , 0) 
+                            + 1)
+            ''')
+            self.cur.commit()
+            
+    def registracija(self):
+            ime = input("Unesi ime: ")
+            email = input("Unesi email: ")
+            lozinka = input("Unesi lozinku: ")
+            kontakt = input("Unesi kontakt broj: ")
+            today = date.today() #danasnji datum
+
+            hash_object = hashlib.md5(lozinka.encode())
+            safelozinka = hash_object.hexdigest()
+            
+            self.cur.execute("INSERT INTO users (name, email, password, kontakt, created_at) VALUES (?,?,?,?,?)",(ime, email, safelozinka, kontakt, today))
+            self.con.commit()
+
+            if(self.cur.rowcount>0):
+                    print("Podatak je zapisan")
+            
+    def forgot_password(self):
         
-        cur = con.cursor()
-        row=cur.execute("SELECT id, password FROM users WHERE name = ?", (ime,))
-        row=cur.fetchone()
-        uid = row[0]
-        password = row[1]
-
-        if(cur.rowcount<=0):
-            print(uid, password)
-            return
-        
-        print("Korisnik postoji i prijavljen \n")
-        now = date.today()
-
-        logins = 0
-
-        cur.execute('''UPDATE logins 
-                    SET logins = ?,
-                    last_login = ?
-                    WHERE uid = ?
-                    ''', (logins+1, now, uid))
-
-        cur.execute('''INSERT INTO logins
-                    (logins, last_login, uid)
-                    VALUES (?, ?, ?)
-                    ''', (logins+1, now, uid))
-
-        cur.execute(f'''INSERT OR REPLACE INTO logins 
-                    (logins, last_login, uid) 
-                    VALUES (
-                        (SELECT logins FROM logins WHERE Name = "SearchName"),
-                        {now},
-                        IFNULL(
-                            (SELECT COUNT FROM logins WHERE Name = "SearchName")
-                            , 0) 
-                        + 1)
-        ''')
-        cur.commit()
-
-        
-def registracija():
-        ime = input("Unesi ime: ")
         email = input("Unesi email: ")
-        lozinka = input("Unesi lozinku: ")
-        kontakt = input("Unesi kontakt broj: ")
-        today = date.today() #danasnji datum
-
-        hash_object = hashlib.md5(lozinka.encode())
-        safelozinka = hash_object.hexdigest()
-
-        cur = con.cursor()
         
-        cur.execute("INSERT INTO users (name, email, password, kontakt, created_at) VALUES (?,?,?,?,?)",(ime, email, safelozinka, kontakt, today))
-        con.commit()
-
-
-        if(cur.rowcount>0):
-                print("Podatak je zapisan")
-                
-        con.close()
+        self.cur.execute("SELECT id FROM users WHERE email=?", (email,))
         
-def forgot_password():
-    cur = con.cursor()
-    
-    email = input("Unesi email: ")
-    
-    cur.execute("SELECT id FROM users WHERE email=?", (email,))
-    
-    row = cur.fetchone()
-    
-    if(row == None):
-        print("Korisnik nije pronađen!")
-        return
+        row = self.cur.fetchone()
+        
+        if(row == None):
+            print("Korisnik nije pronađen!")
+            return
 
-    uid = row[0]
-    
-    curr_time = int(time.time())
-    plus_30_time = datetime.now() + timedelta(minutes=30)
+        uid = row[0]
+        
+        self.curr_time = int(time.time())
+        plus_30_time = datetime.now() + timedelta(minutes=30)
 
-    hash_object = hashlib.md5(str(curr_time).encode())
-    hashed_time = hash_object.hexdigest()
-    
-    cur.execute("INSERT INTO forgot_password (hash, valid_until, uid) VALUES (?,?,?)", (hashed_time, plus_30_time, uid))
-    con.commit()
+        hash_object = hashlib.md5(str(self.curr_time).encode())
+        hashed_time = hash_object.hexdigest()
+        
+        self.cur.execute("INSERT INTO forgot_password (hash, valid_until, uid) VALUES (?,?,?)", (hashed_time, plus_30_time, uid))
+        self.con.commit()
 
-    if(cur.rowcount>0):
-        print("Hash:", hashed_time)
-    
-    con.close()
+        if(self.cur.rowcount>0):
+            print("Hash:", hashed_time)
 
-def main():
+    def reset_password(self, hash):
+        email = input("Unesi email: ")
+        hash = input("Unesi hash: ")
+
+        new_pass = input("Unesi novu lozinku: ")
+        new_pass_again = input("Potvrdi novu lozinku: ")
+
+        if (new_pass != new_pass_again):
+            print("Nove lozinke se ne podudaraju.")
+            return
+
+        self.cur.execute("SELECT uid FROM users WHERE email=?", (email, ))
+        row = self.cur.fetchone()
+        
+        if(row == None):
+            print("Korisnik nije pronađen!")
+            return
+
+        uid = row[0]
+
+            # work on this
+
+        self.cur.execute("UPDATE users SET password=? WHERE id=?", (new_pass, uid))
+
+        self.cur.execute("SELECT id, hash FROM forgot_password WHERE uid=?", (uid, ))
+
+        hash_id=0
+        self.cur.execute("DELETE FROM forgot_password WHERE id=?", (hash_id))
+        self.con.commit()
+
+
+if __name__=="__main__":
     print("Dobrodošli u Unidu sustav!")
     print("Za prijavu upišite broj 1, za registraciju broj 2, za zaboravljenu lozinku broj 3:")
 
     unos = 0
+    acceptable = [1,2,3]
 
-    while(unos!=1 and unos!=2 and unos!=3):
+    while(unos not in acceptable):
         unos = int(input("Unesite broj: "))
 
-    if(unos==3):
-        forgot_password()
+    acc = Account()
 
-    if(unos==2):
-        registracija()
-        
     if(unos==1):
-        login()
-
-
-if __name__=="__main__":
-    main()
+        acc.login()
+    if(unos==2):
+        acc.registracija()
+    if(unos==3):
+        acc.forgot_password()
